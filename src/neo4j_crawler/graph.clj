a1f59da135a74f247665e37c2f482f2d98c0fe13
Max Gonzih <gonzih@gmail.com>
2014-05-28 12:27:05 +0200

    Implement relations in graph

--- src/neo4j_crawler/graph.clj
+++ src/neo4j_crawler/graph.clj
@@ -1,29 +1,48 @@
-(ns neo4j-crawler.graph)
+(ns neo4j-crawler.graph
+  (:require [clojure.set :refer [union]]))
 
 (defrecord Node [id args])
-(defrecord Relation [source targets])
+(defrecord Relation [source target args])
 
 (defprotocol GraphP
-  (create-node [this id args])
-  (create-rel [this source target]))
+  (merge-graphs [this other])
+  (create-node [this id]
+               [this id args])
+  (create-rel [this source target]
+              [this source target args]))
 
 (defrecord Graph [nodes relations]
   GraphP
+  (merge-graphs [this other]
+    (Graph. (union (:nodes this)
+                   (:nodes other))
+            (union (:relations this)
+                   (:relations other))))
+  (create-node [this id]
+    (create-node this id {}))
   (create-node [this id args]
     (assoc this :nodes
            (conj (:nodes this)
                  (Node. id args))))
   (create-rel [this source target]
+    (create-rel this source target {}))
+  (create-rel [this source target args]
     (assoc this :relations
-           (merge-with conj
-                       (:relations this)
-                       {source #{target}
-                        target #{source}}))))
+           (conj
+             (:relations this)
+             (Relation. source target args)
+             (Relation. target source args)))))
 
 (defn init-graph []
-  (Graph. #{} {}))
+  (Graph. #{} #{}))
 
-(-> (init-graph)
-    (create-node "page" {})
-    (create-node "page2" {})
-    (create-rel "page" "page2"))
+(let [g1 (-> (init-graph)
+             (create-node "page" )
+             (create-node "page2" {:title "A"})
+             (create-rel "page" "page2"))
+      g2 (-> (init-graph)
+             (create-node "booboo")
+             (create-node "meemee")
+             (create-node "page")
+             (create-rel "page" "page2"))]
+  (merge-graphs g1 g2))
