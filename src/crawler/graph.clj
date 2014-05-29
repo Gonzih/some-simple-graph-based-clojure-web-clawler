2ca2d636d4f5ff9d52a958ae27fd591395deafc5
Max Gonzih <gonzih@gmail.com>
2014-05-29 14:10:30 +0200

    Count edges from and to node

--- src/crawler/graph.clj
+++ src/crawler/graph.clj
@@ -9,21 +9,26 @@
   (create-node [this id]
                [this id args])
   (create-rel [this source target]
-              [this source target args]))
+              [this source target args])
+  (edges-from [this node])
+  (edges-to [this node]))
 
 (defrecord Graph [nodes edges]
   GraphP
+
   (merge-graphs [this other]
     (Graph. (union (:nodes this)
                    (:nodes other))
             (union (:edges this)
                    (:edges other))))
+
   (create-node [this id]
     (create-node this id {}))
   (create-node [this id args]
     (assoc this :nodes
            (conj (:nodes this)
                  (Node. id args))))
+
   (create-rel [this source target]
     (create-rel this source target {}))
   (create-rel [this source target args]
@@ -31,7 +36,15 @@
            (conj
              (:edges this)
              (Edge. (str source "->" target)
-                    source target args)))))
+                    source target args))))
+
+  (edges-from [this node]
+    (filter #(= (:source %) (:id node))
+            (:edges this)))
+
+  (edges-to [this node]
+    (filter #(= (:target %) (:id node))
+            (:edges this))))
 
 (defn init-graph []
   (Graph. #{} #{}))
--- src/crawler/json.clj
+++ src/crawler/json.clj
@@ -1,12 +1,16 @@
 (ns crawler.json
-  (:require [cheshire.core :as json]))
+  (:require [cheshire.core :as json]
+            [crawler.graph :as graph]))
 
-(defn prepare-node [node]
+(defn prepare-node [input-graph node]
   (-> node
       (assoc :x (rand 20))
       (assoc :y (rand 20))
-      (assoc :size (rand 5))
-      (assoc :color "#f00")
+      (assoc :size (+ (count (graph/edges-from input-graph node))
+                      (count (graph/edges-to   input-graph node))))
+      (assoc :color (if (= 200 (-> node :args :response-code))
+                      "#0f0"
+                      "#f00"))
       (merge (:args node))))
 
 (defn prepare-edge [edge]
@@ -27,11 +31,11 @@
                (conj cache cache-value)))
       result)))
 
-(defn graph->json [graph]
-  (json/generate-string (-> graph
+(defn graph->json [input-graph]
+  (json/generate-string (-> input-graph
                             (assoc :nodes
-                                   (map prepare-node
-                                        (distinct-by :id (:nodes graph))))
+                                   (map (partial prepare-node input-graph)
+                                        (distinct-by :id (:nodes input-graph))))
                             (assoc :edges
                                    (map prepare-edge
-                                        (distinct-by :id (:edges graph)))))))
+                                        (distinct-by :id (:edges input-graph)))))))
